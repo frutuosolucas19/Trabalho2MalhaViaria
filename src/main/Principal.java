a5ab95dc310362e1abb1483a7ad9f176e6773875
matheus-maas <matheus.h.maas@hotmail.com>
2022-09-20 22:38:40 -0300

    Criando as viewMenuMalha e viewSimuladorMalha

---
+++ src/main/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+import view.viewMenuMalha;
+
+/**
+ *
+ * @author matheus
+ */
+public class Principal {
+
+  
+    public static void main(String[] args) {
+        viewMenuMalha viewmenumalha = new viewMenuMalha();
+        
+        viewmenumalha.setVisible(true);
+    }
+    
+}
